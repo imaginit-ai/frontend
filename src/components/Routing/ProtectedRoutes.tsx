267186e4b35fd2b4ba954bd826d9bb1aa198c886
Krushay Bhavsar <krushaybhavsar@gmail.com>
2024-11-11 00:55:54 -0500

    Updated Auth

--- src/components/Routing/ProtectedRoutes.tsx
+++ src/components/Routing/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import { SiteScreens } from "@/types";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoutes = () => {
-  const isAuthenticated = true; // TODO: Implement authentication
+  const isAuthenticated = false; // TODO: Implement authentication
 
   return isAuthenticated ? (
     <Outlet />
